ee7efbd8896f2e97cacae5e1a75fddcc326addc3
Emilio Cazares Borbon <126736863+3m1l1032@users.noreply.github.com>
2026-05-23 20:35:43 -0700

    first implementation of insertion sort

---
+++ cpp/include/insertion_sort.hpp
@@ -0,0 +1,89 @@
+/* 
+ * Emilio Cazares Borbon, 2026
+ * 
+ * Header file for insertion sort, using standard vectors.
+ * 
+ * Reference algorithm: 2.1 Intro to Algorithms
+ */
+
+#ifndef __INSERTION_SORT_HPP__
+#define __INSERTION_SORT_HPP__
+
+#include <vector>
+
+template <typename T>
+class InsertionSort
+{
+public:
+    InsertionSort ();
+    InsertionSort (std::vector<T> data);
+    InsertionSort (const InsertionSort<T>& other);
+    ~InsertionSort ();
+
+    void sortForward (int steps = 1);
+    
+    std::vector<T> data;
+
+private:
+    // algorithm intermediate
+    size_t i;
+
+    void sortForIter ();
+};
+
+template <typename T>
+InsertionSort<T>::InsertionSort ()
+{
+    i = 1;
+}
+
+template <typename T>
+InsertionSort<T>::InsertionSort (std::vector<T> data)
+{
+    i = 1; 
+    this->data = data;
+}
+
+template <typename T>
+InsertionSort<T>::InsertionSort (const InsertionSort<T>& other)
+{
+    i = 1;
+    this->data = other.data;
+}
+
+template <typename T>
+InsertionSort<T>::~InsertionSort ()
+{
+    data.clear();
+}
+
+template <typename T>
+void InsertionSort<T>::sortForward (int steps)
+{
+    
+    for (int i = 0; i < steps; i++)
+    {
+        if (i == data.size())
+            return;
+
+        sortForIter ();
+    }
+}
+
+template <typename T>
+void InsertionSort<T>::sortForIter ()
+{
+    T key = data[i];
+
+    size_t j = i - 1;
+
+    while (j > 0 && data[j] > key)
+    {
+        data[j + 1] = data[j];
+        j--;
+    }
+
+    data[j + 1] = key;
+}
+
+#endif /* __INSERTION_SORT_HPP__  */
